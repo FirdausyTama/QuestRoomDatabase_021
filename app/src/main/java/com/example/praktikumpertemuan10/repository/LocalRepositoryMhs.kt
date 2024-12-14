0a2c2a8467ce25c8cb8ceecab1c02c3da67676b3
FirdausyTama <atama.firdausy@gmail.com>
2024-12-14 16:22:01 +0700

    membuat code agar terhubung ke mahasiswadao dan repositoryMhs

--- app/src/main/java/com/example/praktikumpertemuan10/repository/LocalRepositoryMhs.kt
+++ app/src/main/java/com/example/praktikumpertemuan10/repository/LocalRepositoryMhs.kt
@@ -2,11 +2,31 @@ package com.example.praktikumpertemuan10.repository
 
 import com.example.praktikumpertemuan10.data.dao.MahasiswaDao
 import com.example.praktikumpertemuan10.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
+
+
 
 class LocalRepositoryMhs(
     private val mahasiswaDao: MahasiswaDao
 ) : RepositoryMhs {
+
     override suspend fun insertMhs(mahasiswa: Mahasiswa) {
-        mahasiswaDao.insertMahasiswa(mahasiswa)
+        mahasiswaDao.insertMahasiswa(mahasiswa) // Perbaiki jika ada typo di sini
+    }
+
+    override fun getAllMhs(): Flow<List<Mahasiswa>> {
+        return mahasiswaDao.getAllMahasiswa()
+    }
+
+    override fun getMhs(nim: String): Flow<Mahasiswa> {
+        return mahasiswaDao.getMahasiswa(nim = nim)
+    }
+
+    override suspend fun deleteMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.deleteMahasiswa(mahasiswa)
+    }
+
+    override suspend fun updateMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.updateMahasiswa(mahasiswa)
     }
 }
