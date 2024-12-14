cdcf84d87493e3b570f4f5da558d8b44fef4def6
FirdausyTama <atama.firdausy@gmail.com>
2024-12-14 21:03:01 +0700

    membuat fungsi mahasiswa.toDetailUiEvent, dan membuat statein

--- app/src/main/java/com/example/praktikumpertemuan10/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/praktikumpertemuan10/ui/viewmodel/DetailMhsViewModel.kt
@@ -2,12 +2,18 @@ package com.example.praktikumpertemuan10.ui.viewmodel
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.praktikumpertemuan10.data.entity.Mahasiswa
 import com.example.praktikumpertemuan10.repository.RepositoryMhs
 import com.example.praktikumpertemuan10.ui.navigation.DestinasiDetail
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 
 class DetailMhsViewModel (
     savedStateHandle: SavedStateHandle,
@@ -33,10 +39,17 @@ class DetailMhsViewModel (
                 DetailUiState(
                     isLoading = false,
                     isError = true,
-                    errorMessage = it.message ?: "Terjadi Kesalahan",
+                    errorMessage = it.message ?: "Terjadi kesalahan",
                 )
             )
         }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(2000),
+            initialValue = DetailUiState(
+                isLoading = true,
+            ),
+        )
 
 }
 
@@ -56,3 +69,15 @@ data class DetailUiState(
 /*
 * Data class untuk menampung data yang akan di tampilkan di ui
  */
+
+//memindahkan data dari entity ke ui
+fun Mahasiswa.toDetailUiEvent(): MahasiswaEvent {
+    return MahasiswaEvent(
+        nim = nim,
+        nama = nama,
+        jenisKelamin = jenisKelamin,
+        alamat = alamat,
+        kelas = kelas,
+        angkatan = angkatan
+    )
+}
