84ec9cfb7e2b7b656043b4efd8175d22e613f8e4
FirdausyTama <atama.firdausy@gmail.com>
2024-12-14 17:46:01 +0700

    membuat class detailmhsviewmodel dan data claasnya

--- app/src/main/java/com/example/praktikumpertemuan10/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/praktikumpertemuan10/ui/viewmodel/DetailMhsViewModel.kt
@@ -1,4 +1,58 @@
 package com.example.praktikumpertemuan10.ui.viewmodel
 
-class DetailMhsViewModel {
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.praktikumpertemuan10.repository.RepositoryMhs
+import com.example.praktikumpertemuan10.ui.navigation.DestinasiDetail
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+
+class DetailMhsViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMhs: RepositoryMhs,
+
+    ) : ViewModel(){
+        private val _nim: String = checkNotNull(savedStateHandle[DestinasiDetail.NIM])
+
+    val detailUiState: StateFlow<DetailUiState> = repositoryMhs.getMhs(_nim)
+        .filterNotNull()
+        .map {
+            DetailUiState(
+                detailUiEvent = it.toDetailUiEvent(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(DetailUiState(isLoading = true))
+            delay(600)
+        }
+        .catch {
+            emit(
+                DetailUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan",
+                )
+            )
+        }
+
 }
+
+data class DetailUiState(
+    val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+
+){
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == MahasiswaEvent()
+    val isUiEventNotEmpety: Boolean
+        get() = detailUiEvent != MahasiswaEvent()
+}
+
+/*
+* Data class untuk menampung data yang akan di tampilkan di ui
+ */
